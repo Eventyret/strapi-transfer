29a4a1b3ccaa87c23cd3ba118d943c0e52a39995
Simen Daehlin <simen.daehlin@virginmedia.co.uk>
2022-01-25 20:58:25 +0000

    Added checks for not found and no permissions

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ const { checkAPI } = require('./core/transfer');
 
 const input = cli.input;
 const flags = cli.flags;
-const { clear, debug, retry } = flags;
+const { clear, debug } = flags;
 
 (async () => {
 	init({ clear });
 	input.includes(`help`) && cli.showHelp(0);
-	input.includes(`retry`) && ask();
-	!input.includes(`retry`) && checkAPI();
+	input.includes(`permissions`) && ask('noPermissions');
+	input.includes(`notfound`) && ask('notFound');
+	input.length === 0 && checkAPI();
 	debug && log(flags);
 })();
